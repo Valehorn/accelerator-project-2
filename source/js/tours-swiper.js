aa0b8e4200b4e306fcefff87f8757ade11349a43
Пакри <pakri@yandex.ru>
2024-08-16 02:31:53 +0300

    Починил 2ую прокрутку слайдов в блоке tours при нажатии на кнопки навигации.

--- source/js/tours-swiper.js
+++ source/js/tours-swiper.js
@@ -1,71 +1,67 @@
 import { Swiper } from 'swiper/bundle';
-import { Navigation } from 'swiper/modules';
 import 'swiper/scss';
 
 const toursSwiperContainer = document.querySelector('.tours__swiper');
 
 const initSwiperTours = () => {
-  const breakpoint = window.innerWidth;
-  const toursSwiper = new Swiper(toursSwiperContainer, {
-    modules: [Navigation],
-    speed: 900,
-    spaceBetween: 20,
-    slidesPerView: 1,
-    loop: false,
-    lockClass: '.swiper__button--disabled',
+  const swiperInstance = new Swiper(toursSwiperContainer, {
     navigation: {
       nextEl: '.tours__swiper-button-next',
       prevEl: '.tours__swiper-button-prev',
     },
+    speed: 900,
+    loop: false,
+    slidesPerView: 1,
+    allowTouchMove: true,
+
     breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+
       768: {
         slidesPerView: 2,
         spaceBetween: 18,
       },
+
       1440: {
         slidesPerView: 3,
         spaceBetween: 30,
+        allowTouchMove: false
       }
     },
-    allowTouchMove: breakpoint < 1439,
     on: {
-      slideChange: () => {
-        updateToursButtons();
+      slideChange() {
+        updateNavigationButtons(swiperInstance);
       },
-      reachEnd: () => {
-        updateToursButtons();
-      },
-      reachBeginning: () => {
-        updateToursButtons();
+      slideNextTransitionStart() {
+        updateNavigationButtons(swiperInstance);
       }
     }
   });
+  updateNavigationButtons(swiperInstance);
 
-  function updateToursButtons() {
+  function updateNavigationButtons(swiper) {
     const prevButton = document.querySelector('.tours__swiper-button-prev');
     const nextButton = document.querySelector('.tours__swiper-button-next');
 
-    if (toursSwiper.isBeginning) {
-      prevButton.classList.add('swiper__buttons--disabled');
+    if (swiper.isBeginning) {
+      prevButton.classList.add('swiper__button--disabled');
       prevButton.disabled = true;
-      toursSwiper.allowSlidePrev = false;
     } else {
-      prevButton.classList.remove('swiper__buttons--disabled');
+      prevButton.classList.remove('swiper__button--disabled');
       prevButton.disabled = false;
-      toursSwiper.allowSlidePrev = true;
     }
 
-    if (toursSwiper.isEnd) {
-      nextButton.classList.add('swiper__buttons--disabled');
+    if (swiper.isEnd) {
+      nextButton.classList.add('swiper__button--disabled');
       nextButton.disabled = true;
-      toursSwiper.allowSlideNext = false;
     } else {
-      nextButton.classList.remove('swiper__buttons--disabled');
+      nextButton.classList.remove('swiper__button--disabled');
       nextButton.disabled = false;
-      toursSwiper.allowSlideNext = true;
     }
   }
-  updateToursButtons();
 };
 
 export { initSwiperTours };
