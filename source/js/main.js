47f1201636a09b3f19e58956e43f2b252d99adf2
Пакри <pakri@yandex.ru>
2024-08-17 19:06:13 +0300

    Инициализировал swiper в reviews

--- source/js/main.js
+++ source/js/main.js
@@ -2,12 +2,14 @@ import { burgerMenuToggle } from './burger-menu';
 import { initSwiperHero } from './hero-swiper';
 import { initSwiperTours } from './tours-swiper';
 import { initSwiperTraining } from './training-swiper';
+import { initSwiperReviews } from './reviews-swiper';
 
 const bootStrap = () => {
   burgerMenuToggle();
   initSwiperHero();
   initSwiperTours();
   initSwiperTraining();
+  initSwiperReviews();
 };
 
 bootStrap();
