55c377c7f63791972f5aa8997679176dda9387e7
Пакри <pakri@yandex.ru>
2024-08-22 20:39:59 +0300

    Война с валидацией

--- source/js/form-validate.js
+++ source/js/form-validate.js
@@ -1,47 +1,64 @@
 const form = document.querySelector('.form__card');
 const emailInput = form.querySelector('.form__input--email');
 const phoneInput = form.querySelector('.form__input--phone');
-let isValid = true;
+let isFormSubmitted = false;
 
 const validateEmail = () => {
-  emailInput.setCustomValidity('');
   const emailValue = emailInput.value;
   const emailValidate = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(рф|[a-zA-Z]{2,})$/i;
 
   if (!emailValidate.test(emailValue)) {
     emailInput.setCustomValidity('Email должен содержать латинские буквы, арабские цифры, символы: "-","+", ".". Имя должно быть разделено @. Домен должен быть отделен ".". Допускается домен кириллицей ".рф"');
-    emailInput.reportValidity();
-    emailInput.classList.add('form__input--error');
-    isValid = false;
+    if (isFormSubmitted) {
+      emailInput.classList.add('form__input--error');
+    }
   } else {
+    emailInput.setCustomValidity('');
     emailInput.classList.remove('form__input--error');
   }
 };
 
 const validatePhone = () => {
-  phoneInput.setCustomValidity('');
   const phoneValue = phoneInput.value;
-  const phoneValidate = /^(?:\d[-\d]*){11}$/;
+  const phoneValidate = /^[\d\s-]{11,}$/;
+
   if (!phoneValidate.test(phoneValue)) {
-    phoneInput.setCustomValidity('Телефон может принимать только арабские цифры, пробелы и символ "-", а так же должен быть не менее 11 символов.');
-    phoneInput.reportValidity();
-    phoneInput.classList.add('form__input--error');
-    isValid = false;
+    phoneInput.setCustomValidity('Телефон может принимать только арабские цифры, пробелы и символ "-", а также должен быть не менее 11 символов.');
+    if (isFormSubmitted) {
+      phoneInput.classList.add('form__input--error');
+    }
   } else {
+    phoneInput.setCustomValidity('');
     phoneInput.classList.remove('form__input--error');
+  }
+};
+
+const onInputChange = () => {
+  if (isFormSubmitted) {
+    validateEmail();
+    validatePhone();
   }
 };
 
 const onFormSubmit = (evt) => {
   evt.preventDefault();
-  isValid = true;
+  isFormSubmitted = true;
+
   validateEmail();
   validatePhone();
 
-  if (isValid) {
+  if (form.checkValidity()) {
     form.submit();
     form.reset();
+    isFormSubmitted = false;
+  } else {
+    emailInput.reportValidity();
+    phoneInput.reportValidity();
   }
 };
 
+emailInput.addEventListener('input', onInputChange);
+phoneInput.addEventListener('input', onInputChange);
+
 form.addEventListener('submit', onFormSubmit);
+
