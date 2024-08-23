7546a5cbfd232a29b0c9ada9edb0ba7d0d1f7612
Пакри <pakri@yandex.ru>
2024-08-23 14:38:41 +0300

    Улучшения.

--- source/js/form-validate.js
+++ source/js/form-validate.js
@@ -7,11 +7,9 @@ const validateEmail = () => {
   const emailValue = emailInput.value;
   const emailValidate = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(рф|[a-zA-Z]{2,})$/i;
 
-  if (!emailValidate.test(emailValue)) {
+  if (!emailValidate.test(emailValue) && isFormSubmitted) {
     emailInput.setCustomValidity('Email должен содержать латинские буквы, арабские цифры, символы: "-","+", ".". Имя должно быть разделено @. Домен должен быть отделен ".". Допускается домен кириллицей ".рф"');
-    if (isFormSubmitted) {
-      emailInput.classList.add('form__input--error');
-    }
+    emailInput.classList.add('form__input--error');
   } else {
     emailInput.setCustomValidity('');
     emailInput.classList.remove('form__input--error');
@@ -22,11 +20,9 @@ const validatePhone = () => {
   const phoneValue = phoneInput.value;
   const phoneValidate = /^[\d\s-]{11,}$/;
 
-  if (!phoneValidate.test(phoneValue)) {
+  if (!phoneValidate.test(phoneValue) && isFormSubmitted) {
     phoneInput.setCustomValidity('Телефон может принимать только арабские цифры, пробелы и символ "-", а также должен быть не менее 11 символов.');
-    if (isFormSubmitted) {
-      phoneInput.classList.add('form__input--error');
-    }
+    phoneInput.classList.add('form__input--error');
   } else {
     phoneInput.setCustomValidity('');
     phoneInput.classList.remove('form__input--error');
